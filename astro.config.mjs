91ae9990c31170fca2347ffe218f0a967d5195b2
abdalla980 <100001276@stud.srh-university.de>
2025-12-19 14:02:29 +0100

    Work in progress: update components/layouts

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,13 @@ export default defineConfig({
   base: config.site.base_path ? config.site.base_path : "/",
   trailingSlash: config.site.trailing_slash ? "always" : "never",
   image: { service: sharp() },
-  vite: { plugins: [tailwindcss()] },
+  vite: { 
+    plugins: [tailwindcss()],
+    build: {
+      cssMinify: true,
+      minify: "esbuild",
+    },
+  },
   integrations: [
     react(),
     sitemap(),
